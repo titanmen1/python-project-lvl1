0d2b7132ea7eaa838b06c8e6e121c7bc305420d1
Artem Arkhipov <zaq12123@mail.ru>
2021-01-13 23:18:53 +0300

    update CI v1.8

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -5,7 +5,6 @@ import prompt
 
 def welcome_user():
     """Welcome."""
-    print('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')  # noqa: WPS421
     name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
-
+    print('Hello, {}!'.format(name))  # noqa: WPS421, P101
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+"""3."""
 from brain_games.cli import welcome_user
 
 
 def main():
+    """4."""
     welcome_user()
 
 
