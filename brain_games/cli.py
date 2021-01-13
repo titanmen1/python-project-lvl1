74616eb5bdf5cc83fc2dc73ffcd05606180eecb9
Artem Arkhipov <zaq12123@mail.ru>
2021-01-13 22:23:51 +0300

    add check lint in CI

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+"""CLI."""
 import prompt
 
 
 def welcome_user():
+    """Welcome."""
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(name))
